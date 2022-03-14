8a6500ce6b1ef9c48076d4b9408b59cbdc535761
JamesLiuToronto <jamesliu_8@yahoo.com>
2022-03-13 22:27:07 -0400

    Message and validation

--- myvalidation-spring-boot-starter/src/main/java/org/peak/common/myvalidation/exception/BusinessExceptionHandler.java
+++ myvalidation-spring-boot-starter/src/main/java/org/peak/common/myvalidation/exception/BusinessExceptionHandler.java
@@ -1,15 +1,16 @@
 package org.peak.common.myvalidation.exception;
 
 import org.peak.common.myvalidation.errormessage.ErrorMessage;
-import org.peak.common.token.AuthorizeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.ValidationException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,6 +42,24 @@ public class BusinessExceptionHandler {
         return new ResponseEntity<>((new ArrayList<String>(Arrays.asList(error))), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public ResponseEntity<List> processUnmergeException(final MethodArgumentNotValidException ex) {
+
+        final List<String> errors = new ArrayList<String>();
+        for (final FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + ErrorMessage.toLocale(ex.getMessage()));
+        }
+        for (final ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + ErrorMessage.toLocale(ex.getMessage()));
+        }
+
+        String error = String.join(", ", errors);
+        ex.printStackTrace() ;
+        return new ResponseEntity<>((new ArrayList<String>(Arrays.asList(error))), HttpStatus.BAD_REQUEST);
+    }
+
+/*
     @ExceptionHandler({EntityNotFoundException.class})
     @ResponseBody
     //public ResponseEntity<List> processUnmergeException(final MethodArgumentNotValidException ex) {
@@ -49,5 +68,6 @@ public class BusinessExceptionHandler {
         String error = ErrorMessage.toLocale(ex.getMessage()) ;
         ex.printStackTrace() ;
         return new ResponseEntity<>((new ArrayList<String>(Arrays.asList(error))), HttpStatus.NOT_FOUND);
-    }
+    }*/
+
 }
